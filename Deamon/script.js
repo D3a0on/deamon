35992438b9f784bd26dab7316bd90dec4044de05
D3a0on <77934854+D3a0on@users.noreply.github.com>
2021-01-24 20:44:56 -0300

    Update script.js

--- Deamon/script.js
+++ Deamon/script.js
@@ -2,15 +2,21 @@ const key = document.getElementById('key')
 const run = document.getElementById('run')
 
 const server_link = ``
-function login(data){
-
-    // ...
-}
-
 
 
 run.addEventListener('click', ()=> {
-    fetch(`${server_link}:${key}`)
-    .then(response => response.json())
-     .then(data => login(data))
+    fetch(server_link).then(response => response.Json()).then(data, ()=> {
+    
+    if(data["login"] == false){
+    
+        alert('Chave Invalida')
+        
+    }
+    if(data["login"] == true){
+    
+        window.location.replace('https://painel.d3a0on.vercel.app/')
+    }
+    })
+
+
 })
