14f036a3dd9d1a9a6168700fb4c35984b9b3e6cc
D3a0on <77934854+D3a0on@users.noreply.github.com>
2021-01-27 23:25:07 -0300

    Update script.js

--- Deamon/script.js
+++ Deamon/script.js
@@ -1,12 +1,31 @@
 const key = document.getElementById('key')
 const run = document.getElementById('run')
 
-const server_link = ``
-
 run.addEventListener('click', ()=> {
-    fetch('https://api-git-main.d3a0on.vercel.app/api')
     
-    })
+    fetch("https://api-git-main.d3a0on.vercel.app/api", { 
+      
+    // Adding method type 
+    method: "POST", 
+      
+    // Adding body or contents to send 
+    body: JSON.stringify({ 
+        title: "foo", 
+        body: `${key.value}`, 
+        userId: 1 
+    }), 
+      
+    // Adding headers to the request 
+    headers: { 
+        "Content-type": "application/json; charset=UTF-8"
+    } 
+}) 
+  
+// Converting to JSON 
+.then(response => response.json()) 
+  
+// Displaying results to console 
+.then(json => console.log(json)); 
 
 
 })
